b9b6a599cfedd58cece793df03d833cad9a45723
MatheusNascimento93 <theu_1993@hotmail.com>
2021-05-25 16:06:00 -0300

    Projeto Pagina do Orlando City Concluido!

--- js/efeitos.js
+++ js/efeitos.js
@@ -40,4 +40,14 @@ $(document).ready(function () {
 		}, 1000);
 		event.preventDefault();
 	});
+	$("#btn-bars").on("click", function () {
+		$("header").toggleClass("open-menu");
+	});
+	$("#menu-mobile-mask, .btn-close").on("click", function () {
+		$("header").removeClass("open-menu");
+	});
+	$("#btn-search").on("click", function () {
+		$("header").toggleClass("open-search");
+		$("input-search-mobile").focus();
+	});
 });
